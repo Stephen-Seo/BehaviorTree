db4fdd3cbc8300705f09470df55a693bcc10e16e
Stephen Seo <seo.disparate@gmail.com>
2017-02-01 16:50:44 +0900

    Simplified PriorityNode.continueAction()

--- src/BT/PriorityNode.cpp
+++ src/BT/PriorityNode.cpp
@@ -39,40 +39,20 @@ BT::BehaviorNode::State BT::PriorityNode::continueAction()
     State cState{};
     for(std::size_t i = state.lastRunningIndex; i < children.size(); ++i)
     {
-        if(i == state.lastRunningIndex)
+        cState = children[i]->activate();
+        switch(cState.stateType)
         {
-            cState = children[i]->activate();
-            switch(cState.stateType)
-            {
-            case State::READY_SUCCESS:
-                state.stateType = State::READY_SUCCESS;
-                return state;
-            case State::RUNNING:
-                // state should already be set to this child's info
-                return state;
-            case State::FAILED:
-            case State::ERROR:
-            default:
-                break;
-            }
-        }
-        else
-        {
-            cState = children[i]->activate();
-            switch(cState.stateType)
-            {
-            case State::READY_SUCCESS:
-                state.stateType = State::READY_SUCCESS;
-                return state;
-            case State::RUNNING:
-                state.stateType = State::RUNNING;
-                state.lastRunningIndex = i;
-                return state;
-            case State::FAILED:
-            case State::ERROR:
-            default:
-                break;
-            }
+        case State::READY_SUCCESS:
+            state.stateType = State::READY_SUCCESS;
+            return state;
+        case State::RUNNING:
+            state.stateType = State::RUNNING;
+            state.lastRunningIndex = i;
+            return state;
+        case State::FAILED:
+        case State::ERROR:
+        default:
+            break;
         }
     }
     state.stateType = State::READY_SUCCESS;
